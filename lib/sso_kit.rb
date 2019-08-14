6b927069d7113eae753b8d2ab974d99145d596ca
Emilio <zhangjiayuan93@hotmail.com>
2019-08-14 14:55:14 +0800

    ADD: request token

--- lib/sso_kit.rb
+++ lib/sso_kit.rb
@@ -4,12 +4,18 @@ class SsoKit
     @binding = binding
   end
 
+  # 验证 token
   def verify_token
-    incoming_request = eval('request', @binding)
-    token = incoming_request.cookies['token']
+    cookies = eval('cookies', @binding)
+    token = cookies['token']
     # TODO (zhangjiayuan): 考虑对 raise 的异常进行归类整理
-    raise 'cookie 未带有 token' if token.blank?
-    raise '验证 token 失败' unless verify(token)
+    raise '验证 token 失败' if token.blank? || !verify(token)
+    true
+  end
+
+  # 请求 token
+  def self.request_token(some_id)
+    token(some_id)
   end
 
   private
@@ -19,11 +25,21 @@ class SsoKit
     url = "https://xiguacity.cn/server/auth/touch-session"
     data = { token: token }
     response = HttpHandler.new(url, 'post', data).run
-    result = JSON.parse response.body
     return false if response.code != 200
+    result = JSON.parse response.body
     # 防止中间人攻击，验证返回的 token 是否与传出的一致
     return false if token != result['token']
     true
+  end
+
+  def token(some_id)
+    # TODO (zhangjiayuan): use uri config file
+    url = "https://xiguacity.cn/server/auth/create-session"
+    data = { some_id: some_id }
+    response = HttpHandler.new(url, 'post', data).run
+    return nil if response.code != 200
+    result = JSON.parse response.body
+    result['token']
   end
 end
 
